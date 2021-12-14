8f3bb127f6c89f883cf3ac742401583396760484
admvicli <admvicli@gmail.com>
2021-12-14 03:47:17 -0300

    Função "Adicionar Contatos" implantada.

--- functions/databaseFunctions.py
+++ functions/databaseFunctions.py
@@ -1,13 +1,47 @@
 import sqlite3
 
+def connectDatabase():
+    try:
+        database = sqlite3.connect('database.db')
+        database.close()
+        return True
+    except sqlite3.Error as error:
+        return print(f'Ocorreu um erro. Erro: {error}')
+
+def closeDatabase():
+    database = sqlite3.connect('database.db')
+    database.close()
+
 def checkDatabase():
     try:
         database = sqlite3.connect('database.db')
         cursor = database.cursor()
-        cursor.execute("""CREATE TABLE contacts (name text, ddd integer, number integer)""")
+        cursor.execute("""CREATE TABLE contacts (name text, ddd integer, number integer, id integer)""")
         database.commit()
         database.close()
     except sqlite3.OperationalError:
         print("[DATABASE] ☑️ ")
     except:
         print('Ocorreu algum erro na Database.')
+
+def countDatabase():
+    try:
+        database = sqlite3.connect('database.db')
+        cursor = database.cursor()
+        cursor.execute("""SELECT COUNT(*) FROM contacts""")
+        count = cursor.fetchone()
+        database.close()
+        return count[0]
+    except:
+        print('Ocorreu algum erro ao ver contatos cadastrados.')
+
+def addItemDatabase(name, ddd, number):
+    try:
+        database = sqlite3.connect('database.db')
+        cursor = database.cursor()
+        cursor.execute("""INSERT INTO contacts VALUES (?, ?, ?, ?)""", (name, ddd, number, countDatabase() + 1))
+        database.commit()
+        database.close()
+    except sqlite3.Error as error:
+        print(f'Ocorreu algum erro ao adicionar contato. Erro: {error}')
+
--- functions/mainFunctions.py
+++ functions/mainFunctions.py
@@ -1,3 +1,9 @@
+import sqlite3
+
+database = sqlite3.connect('database.db')
+cursor = database.cursor()
+
+from functions.databaseFunctions import closeDatabase, addItemDatabase, connectDatabase
 def menuOptions():
     print("""
 1. Adicione um novo contato
@@ -7,35 +13,93 @@ def menuOptions():
 5. Liste todos os contatos
 6. Sair""")
     while True:
-        choice = int(input("Escolha uma opção: "))
-        if choice in range(1,7):
+        choice = input("Escolha uma opção: ")
+        if choice in "123456":
             return choice
         else:
-            print("Opção inválida!")
+            print("Opção inválida. Tente novamente.")
+
+def nameFix(name):
+    return ' '.join(element.capitalize() for element in name.split())
 
 def menuChoice():
     choice = menuOptions()
     match choice:
-        case 1:
+        case '1':
+            connectDatabase()
             addContact()
-        case 2:
+            closeDatabase()
+        case '2':
+            connectDatabase()
             deleteContact()
-        case 3:
+            closeDatabase()
+        case '3':
+            connectDatabase()
             updateContact()
-        case 4:
+            closeDatabase()
+        case '4':
+            connectDatabase()
             searchContact()
-        case 5:
+            closeDatabase()
+        case '5':
+
             listContacts()
-        case 6:
+            closeDatabase()
+        case '6':
+            connectDatabase()
+            from time import sleep
+            print('Saindo do programa...')
+            sleep(1.5)
             exit()
 
 def addContact():
-    print()
+    while True:
+        nameInput = input('Nome do contato: ')
+        if nameInput.strip() == '':
+            print("Nome não pode ser vazio.")
+        else:
+            break
+    name = nameFix(nameInput)
+    while True:
+        ddd = input('Insira o DDD: ')
+        if ddd == '':
+            print("DDD não pode ser vazio.")
+        elif ddd.isdigit() == False:
+            print("DDD deve ser numérico.")
+        elif len(ddd) != 2:
+            print("DDD deve ter 2 dígitos.")
+        elif ddd == '00':
+            print("DDD não pode ser 00.")
+        else:
+            break
+    while True:
+        phone = input('Insira o número de telefone: ')
+        if phone == '':
+            print("Por favor, digite um número válido.")
+        elif phone.isdigit() == False:
+            print("Por favor, digite um número válido.")
+        elif len(phone) != 8 and len(phone) != 9:
+            print("Número deve ter pelo menos 8 ou 9 dígitos.")
+        else:
+            break
+    addItemDatabase(name, ddd, phone)
+    while True:
+        option = input('Deseja adicionar outro contato? (S/N) ').upper().strip()
+        if option not in 'SN':
+            print("Opção inválida. Tente novamente.")
+        if option == 'S':
+            addContact()
+        else:
+            break 
+    
 def deleteContact():
     print()
+
 def updateContact():
     print()
+
 def searchContact():
     print()
+
 def listContacts():
     print()
